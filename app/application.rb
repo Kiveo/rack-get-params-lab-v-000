d6a973eafa75b36eb2b5168c31a6417c683c890f
Maverick Tesini <mavericktesini@gmail.com>
2018-07-04 09:46:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,8 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart == []
         resp.write "Your cart is empty"
-      end
+      else
+        resp.write @@cart
     else
       resp.write "Path Not Found"
     end
